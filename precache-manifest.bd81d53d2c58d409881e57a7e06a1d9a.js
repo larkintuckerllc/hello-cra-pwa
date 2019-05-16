842256ffe3af667e8300163d4bd2d52813d10d93
John Tucker <jtucker@appirio.com>
2019-05-16 18:27:08 -0400

    Updates

---
+++ precache-manifest.bd81d53d2c58d409881e57a7e06a1d9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96664b5c8c3f533804c38eb3d36e12d9",
+    "url": "/hello-cra-pwa/index.html"
+  },
+  {
+    "revision": "edae1c8a469338cdc2a6",
+    "url": "/hello-cra-pwa/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/hello-cra-pwa/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "edae1c8a469338cdc2a6",
+    "url": "/hello-cra-pwa/static/js/main.defeb7db.chunk.js"
+  },
+  {
+    "revision": "1c0c5c226d81a3244797",
+    "url": "/hello-cra-pwa/static/js/runtime~main.2de2f7ec.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hello-cra-pwa/static/media/logo.5d5d9eef.svg"
+  }
+]);
