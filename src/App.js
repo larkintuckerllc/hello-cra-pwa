2362a2186b668c2994acff9ed3290d3c0f266041
John Tucker <jtucker@appirio.com>
2019-05-26 10:45:24 -0400

    Apple icons

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App({ contentCached, updateAvailable }) {
           >
             Learn React (Progressive Web Apps)
           </a>
-          <p>E</p>
+          <p>F</p>
           {updateAvailable && <p>New content is available and will be used when all tabs for this page are closed. See https://bit.ly/CRA-PWA.</p>}
           {contentCached && <p>Content is cached for offline use.</p>}
           <ul>
